c18228e8457f2afa8a4226f9f43c0b1768f64650
guillauime <guillaume.augustoni@gmail.com>
2018-04-04 09:49:42 +0200

    lkjbmkjnsfnbsrfw

--- V1.py
+++ V1.py
@@ -5,20 +5,10 @@ class HMM():
     """ Define an HMM"""
 
     def __init__(self, nbl, nbs, initial, transitions, emissions):
-        if not isinstance(initial, np.ndarray):
-            raise ValueError("initial doit être un array numpy")
-        if not isinstance(transitions, np.ndarray):
-            raise ValueError("transitions doit être un array numpy")
         if not isinstance(emissions, np.ndarray):
             raise ValueError("emissions doit être un array numpy")
-        if np.shape(initial) != (1, nbs):
-            raise ValueError("initial n'a pas la bonne dimension")
-        if np.shape(transitions) != (nbs, nbs):
-            raise ValueError("transitions n'a pas la bonne dimension")
         if np.shape(emissions) != (nbs, nbl):
             raise ValueError("emissions n'a pas la bonne dimension")
-        if not np.isclose(np.array([initial.np.sum()]), np.array([1.0])):
-            raise ValueError("la somme des probabilités initiales doit être 1")
         # The number of letters
         self.nbl = nbl
         # The number of states
@@ -55,4 +45,26 @@ class HMM():
         self.__nbs = nbs
 
     @property
-    def
+    def initial(self):
+        return self.__initial
+
+    @initial.setter
+    def initial(self, initial):
+        if not isinstance(initial, np.ndarray):
+            raise ValueError("initial doit être un array numpy")
+        if np.shape(initial) != (1, self.nbs):
+            raise ValueError("initial n'a pas la bonne dimension")
+        if not np.isclose(np.array([initial.np.sum()]), np.array([1.0])):
+            raise ValueError("la somme des probabilités initiales doit être 1")
+        self.__initial = initial
+
+    @property
+    def transitions(self):
+        return self.__transitions
+
+    @transitions.setter
+    def transitions(self, transitions):
+        if not isinstance(transitions, np.ndarray):
+            raise ValueError("transitions doit être un array numpy")
+        if np.shape(transitions) != (self.nbs, self.nbs):
+            raise ValueError("transitions n'a pas la bonne dimension")
