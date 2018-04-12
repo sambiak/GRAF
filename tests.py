040469ad1dc2fe7ee10950d4e7d9a853e2e93dde
François <franvoisoder@gmail.com>
2018-04-12 14:32:28 +0200

    ajout de test_viterbi

--- tests.py
+++ tests.py
@@ -13,11 +13,11 @@ class TestHMM(unittest.TestCase):
         self.assertEqual(0, HMM.draw_multinomial(np.array([1, 0, 0])))
 
     def test_gen_rand(self):
-        M = HMM.load('test_gen_rand')
+        M = HMM.load('save3.txt')
         self.assertEqual([0, 1, 2, 0, 1, 2, 0, 1, 2], M.gen_rand(9))
 
     def test_pfw(self):
-        M = HMM.load('test_gen_rand')
+        M = HMM.load('save3.txt')
         self.assertEqual(1, M.pfw([0, 1, 2]))
 
     def test_pfw2(self):
@@ -25,7 +25,7 @@ class TestHMM(unittest.TestCase):
         self.assertEqual(0.5, M.pfw([0, 1, 2]))
 
     def test_pbw(self):
-        M = HMM.load('test_gen_rand')
+        M = HMM.load('save3.txt')
         self.assertEqual(1, M.pbw([0, 1, 2]))
 
     def test_pbw2(self):
@@ -33,8 +33,12 @@ class TestHMM(unittest.TestCase):
         self.assertEqual(0.5, M.pbw([0, 1, 2]))
 
     def test_predit(self):
-        M = HMM.load('test_gen_rand')
+        M = HMM.load('save3.txt')
         self.assertEqual(2, M.predit([0, 1]))
+
+    def test_viterbi(self):
+        M = HMM.load('save3.txt')
+        self.assertEqual(([0, 1, 2], 1.0), M.viterbi([0, 1, 2]))
 
 if __name__ == '__main__':
     unittest.main()
