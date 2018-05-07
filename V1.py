745af30cef9fff2064cf72d83f42684de50294e9
François <franvoisoder@gmail.com>
2018-05-07 21:28:41 +0200

    docstrings de draw_multinomial et gen_rand

--- V1.py
+++ V1.py
@@ -11,9 +11,9 @@ class HMM:
 
         :param nbl: nombre de lettres
         :param nbs: nombre d'états
-        :param initial: les probabilitées d'entrée dans le HMM
-        :param transitions: les probabilitées de transition à l'intérieur du HMM
-        :param emissions: les probabilitées d'émissions dans un HMM
+        :param initial: les probabilités d'entrée dans le HMM
+        :param transitions: les probabilités de transition à l'intérieur du HMM
+        :param emissions: les probabilités d'émissions dans un HMM
         """
         self.nbl = nbl
         self.nbs = nbs
@@ -172,6 +172,11 @@ class HMM:
 
     @staticmethod
     def draw_multinomial(L):
+        """
+
+        :param L: une liste de probabilités (sommant à 1)
+        :return: un indice i avec une probabilité de L[i]
+        """
         x = rd.random()
         M = []
         somme = 0
@@ -184,6 +189,11 @@ class HMM:
                 return i
 
     def gen_rand(self, n):
+        """
+
+        :param n: longueur du mot à générer
+        :return: un mot généré à partir du HMM self
+        """
         rd.seed()
         s = HMM.draw_multinomial(self.initial)
         S = [s]
