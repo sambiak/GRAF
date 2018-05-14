5652a76c28d048ab6b86d4f37f8fae0e7e45bc0e
François <franvoisoder@gmail.com>
2018-05-14 17:17:40 +0200

    avancement du projet

--- projet.py
+++ projet.py
@@ -32,7 +32,7 @@ def mots(S):
         Res += [word]
     return Res
 
-def xval(nbFolds, S, nbL, nbSMin, nbSMax, nbIter, nbInit):
+def xval(nbFolds, S, nbL, nbSMin, nbSMax, nbInit):
     n = len(S)
     l = np.random.permutation(n)
     lvOpt = -float('inf')
@@ -44,7 +44,7 @@ def xval(nbFolds, S, nbL, nbSMin, nbSMax, nbIter, nbInit):
             learn = [S[l[j]] for j in range(f1)]
             learn += [S[l[j]] for j in range(f2,n)]
             test = [S[l[j]] for j in range(f1,f2)]
-            h = HMM.HMM.BW3(nbL,nbS,learn,nbIter,nbInit)
+            h = HMM.HMM.BW4_mieux(nbL,nbS,learn,nbInit)
         lv += h.log_vraissemblance(test)
         if lv > lvOpt:
             lvOpt = lv
@@ -58,18 +58,18 @@ print("Nous allons voir une application des HMM pour reconnnaître une langue et
 print("Commençons par récupérer une séquence de mots, les plus courants de la langue anglaise, que vous pouvez voir dan"
       "s le fichier 'anglais2000'", "\n\n")
 S_anglais = sequence_langue('anglais2000')
-print("Génèrons un HMM au hasard et adaptons-le à la génération des mots de cette séquence")
-print("Nous avons choisis de générer un HMM avec 30 états", "\n\n")
+print("Générons un HMM au hasard et adaptons-le à la génération des mots de cette séquence.")
+print("Nous avons choisi de générer un HMM avec 30 états.", "\n\n")
 HMM_anglais1, it = HMM.HMM.BW2_mieux(30, 26, S_anglais)
-print("Nous obtenons un HMM que nous stockons dans le fichier 'HMM_anglais_V1'", "\n\n")
+print("Nous obtenons un HMM que nous stockons dans le fichier 'HMM_anglais_V1'.", "\n\n")
 HMM_anglais1.save('HMM_anglais_V1')
-print("Nous avons mis à jour notre HMM " + str(it) + " fois avant d'obtenir des HMMs avec des vraissemblances stables")
-print("Interressons-nous à la log-vraissemblance de notre séquence de mots anglais dans ce HMM:")
+print("Nous avons mis à jour notre HMM " + str(it) + " fois avant d'obtenir des HMMs avec des vraisemblances stables")
+print("Intéressons-nous à la log-vraisemblance de notre séquence de mots anglais dans ce HMM:")
 logV_anglais1 = HMM_anglais1.log_vraissemblance(S_anglais)
 print(logV_anglais1, "\n\n")
-print("Cela nous permets de générer des mots potentiellement anglais :")
+print("Cela nous permet de générer des mots potentiellement anglais :")
 S = []
-for i in range(25):
-    w = HMM_anglais1.gen_rand(6)[1]
+for i in range(10):
+    w = HMM_anglais1.gen_rand(5)[1]
     S += [w]
 print(mots(S))
